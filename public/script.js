6967940c792a00a666e4ec55ca7f71fefafc2795
William Stewart <william.stewart@caspercollege.edu>
2026-02-03 13:11:09 -0700

    pacman

--- public/script.js
+++ public/script.js
@@ -1,44 +1,97 @@
-function drawSprite(imgSrc, svgElement) {
-  ctx = document.createElement("canvas").getContext("2d");
+// @ts-check
+/// <reference lib="dom" />
 
-  var img1 = new Image();
-  img1.src = imgSrc;
+class Pacman {
+  /**
+   * @param {HTMLElement} targetElement
+   */
+  constructor(targetElement) {
+    this.currentSprite = this.sprites.right;
 
-  img1.onload = function () {
-    svgElement.replaceChildren();
-    ctx.drawImage(img1, 0, 0);
-    const data = ctx.getImageData(0, 0, 16, 16).data;
+    const svgElement = document.createElementNS(
+      "http://www.w3.org/2000/svg",
+      "svg",
+    );
+    svgElement.id = "pacman";
+    svgElement.setAttribute("data-frame", "1");
+    svgElement.setAttribute("width", "32");
+    svgElement.setAttribute("height", "32");
+    svgElement.setAttribute("shape-rendering", "crispEdges");
+    svgElement.setAttribute("image-rendering", "pixelated");
 
-    const pixels = [];
-    for (let i = 0; i < data.length; i += 4) {
-      pixels.push({
-        r: data[i],
-        g: data[i + 1],
-        b: data[i + 2],
-        a: data[i + 3],
-      });
-    }
+    const useElement = document.createElementNS(
+      "http://www.w3.org/2000/svg",
+      "use",
+    );
 
-    for (let y = 0; y < 16; y++) {
-      for (let x = 0; x < 16; x++) {
-        const i = (y * 16 + x) * 4;
-        const r = data[i];
-        const g = data[i + 1];
-        const b = data[i + 2];
-        const a = data[i + 3] / 255;
+    useElement.setAttribute("href", this.currentSprite.href);
+    svgElement.appendChild(useElement);
+    targetElement.replaceWith(svgElement);
 
-        const rect = document.createElementNS(
-          "http://www.w3.org/2000/svg",
-          "rect",
-        );
-        rect.setAttribute("x", x);
-        rect.setAttribute("y", y);
-        rect.setAttribute("width", 1);
-        rect.setAttribute("height", 1);
-        rect.setAttribute("fill", `rgba(${r},${g},${b},${a})`);
+    this.svgElement = svgElement;
+    this.useElement = useElement;
+  }
 
-        svgElement.appendChild(rect);
-      }
-    }
+  sprites = {
+    right: {
+      href: "/spritesheet.svg#pacman-right",
+    },
+    chompRight: {
+      href: "/spritesheet.svg#pacman-chomp-right",
+    },
   };
+
+  chomp() {}
+
+  toggleChomp() {}
+
+  openMouth() {
+    this.currentSprite = this.sprites.chompRight;
+    this.useElement.setAttribute("href", this.currentSprite.href);
+  }
 }
+
+// function drawSprite(imgSrc, svgElement) {
+//   ctx = document.createElement("canvas").getContext("2d");
+
+//   var img1 = new Image();
+//   img1.src = imgSrc;
+
+//   img1.onload = function () {
+//     svgElement.replaceChildren();
+//     ctx.drawImage(img1, 0, 0);
+//     const data = ctx.getImageData(0, 0, 16, 16).data;
+
+//     const pixels = [];
+//     for (let i = 0; i < data.length; i += 4) {
+//       pixels.push({
+//         r: data[i],
+//         g: data[i + 1],
+//         b: data[i + 2],
+//         a: data[i + 3],
+//       });
+//     }
+
+//     for (let y = 0; y < 16; y++) {
+//       for (let x = 0; x < 16; x++) {
+//         const i = (y * 16 + x) * 4;
+//         const r = data[i];
+//         const g = data[i + 1];
+//         const b = data[i + 2];
+//         const a = data[i + 3] / 255;
+
+//         const rect = document.createElementNS(
+//           "http://www.w3.org/2000/svg",
+//           "rect",
+//         );
+//         rect.setAttribute("x", x);
+//         rect.setAttribute("y", y);
+//         rect.setAttribute("width", 1);
+//         rect.setAttribute("height", 1);
+//         rect.setAttribute("fill", `rgba(${r},${g},${b},${a})`);
+
+//         svgElement.appendChild(rect);
+//       }
+//     }
+//   };
+// }
